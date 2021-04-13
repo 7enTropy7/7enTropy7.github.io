9e6566c5587bece13154010f230927771d1587ba
7enTropy7 <menon.uk1998@gmail.com>
2021-04-13 14:42:49 +0530

    contact testing

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -20,7 +20,7 @@ if(isset($_POST['submit'])){
 <body>
 
 <form action="" method="post">
-First Name: <input type="text" name="first_name"><br>
+First Name: <input type="text" name="from_name"><br>
 Email: <input type="text" name="email"><br>
 Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
 <input type="submit" name="submit" value="Submit">
